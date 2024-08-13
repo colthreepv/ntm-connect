6ac4990792554ab5193622f31479e2521c3f92e5
colthreepv <colthreepv@gmail.com>
2024-08-13 18:36:12 +0700

    worst looking navbar ever but it's here!

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useAuthStore } from './login/auth-store'
+import Navbar from './navbar'
 
 export default function Home() {
   const router = useRouter()
@@ -15,19 +16,22 @@ export default function Home() {
   }, [user, loading, router])
 
   return (
-    <main className="container mx-auto">
-      {loading && (
-        <p
-          className="h-4 rounded-full bg-gray-200 dark:bg-neutral-700"
-          style={{ width: '40%' }}
-        ></p>
-      )}
-      {user && (
-        <p className="text-white">
-          Welcome,
-          {user.email}!
-        </p>
-      )}
-    </main>
+    <div>
+      <Navbar />
+      <main className="container mx-auto">
+        {loading && (
+          <p
+            className="h-4 rounded-full bg-gray-200 dark:bg-neutral-700"
+            style={{ width: '40%' }}
+          ></p>
+        )}
+        {user && (
+          <p className="text-white">
+            Welcome,
+            {user.email}!
+          </p>
+        )}
+      </main>
+    </div>
   )
 }
