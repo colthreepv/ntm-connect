d8f5348e4976a02a52fb08e3977680e3ea5207f3
colthreepv <colthreepv@gmail.com>
2024-10-31 05:13:45 +0700

    eslint that makes nextjs happy

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,13 +1,25 @@
 import antfu from '@antfu/eslint-config'
 import prettier from 'eslint-config-prettier'
+import nextPlugin from '@next/eslint-plugin-next'
 
-export default antfu({
-  // Extend Next.js core web vitals rules
-  extends: ['next/core-web-vitals'],
-  react: true,
+export default antfu(
+  {
+    react: true,
 
-  // Add any custom rules or overrides
-  rules: {
-    'node/prefer-global/process': 'off',
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+
+    rules: {
+      'node/prefer-global/process': 'off',
+
+      // Add recommended Next.js rules
+      '@next/next/no-html-link-for-pages': 'error',
+      '@next/next/no-img-element': 'error',
+      '@next/next/no-unwanted-polyfillio': 'error',
+      '@next/next/no-page-custom-font': 'error',
+    },
   },
-}, prettier)
+
+  prettier,
+)
